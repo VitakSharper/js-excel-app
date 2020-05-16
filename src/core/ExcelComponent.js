e56f02d4b0c7056460466c5110480043e4b62afa
VitakSharper <v.banard@gmail.com>
2020-05-16 19:18:24 +0200

    Initial Js Framework setup

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,14 @@
+import {DomListener} from "@core/DomListener";
+
+export class ExcelComponent extends DomListener {
+
+    constructor() {
+        super();
+    }
+
+    // return template of the component
+    toHTML() {
+        return ''
+    }
+
+}
